6d355a133c1134da797a3dc2ef7ef10751df5a82
Bao Tran <tqbbao@gmail.com>
2023-08-22 16:03:27 +0700

    vyssasasdasdsádsas

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.json({
-        msg: '22424001:Tran Quoc Bao -- 22424006:Le Ha Giang   '
+        msg: '22424001:Tran Quoc Bao -- 22424006:Le Ha Giang        '
     })
 })
 
